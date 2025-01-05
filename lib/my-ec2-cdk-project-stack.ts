8239c43336632c353318d747451eef42ae2584b4
Mayur Zilpe - Orient <mayurzilpe@orientindia.net>
2025-01-05 18:34:22 +0530

    Initial commit for EC2 CDK project

---
+++ lib/my-ec2-cdk-project-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export class MyEc2CdkProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, // Default is all AZs in the region
+    });
+
+    // Create an EC2 instance
+    const instance = new ec2.Instance(this, 'MyInstance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+    });
+  }
+}
---
+++ test/my-ec2-cdk-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyEc2CdkProject from '../lib/my-ec2-cdk-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-ec2-cdk-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyEc2CdkProject.MyEc2CdkProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
